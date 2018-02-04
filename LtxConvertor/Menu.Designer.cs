1c5d814959272f30096233e6b72ef76b8bdd5f69
Furyslav <yboj2705@yandex.ru>
2018-02-04 18:20:42 +0300

    first commit

---
+++ LtxConvertor/Menu.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace LtxConvertor
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.buttonFiles = new System.Windows.Forms.Button();
+            this.textBoxFiles = new System.Windows.Forms.TextBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFiles
+            // 
+            this.buttonFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFiles.Location = new System.Drawing.Point(432, 43);
+            this.buttonFiles.Name = "buttonFiles";
+            this.buttonFiles.Size = new System.Drawing.Size(75, 23);
+            this.buttonFiles.TabIndex = 0;
+            this.buttonFiles.Text = "Files";
+            this.buttonFiles.UseVisualStyleBackColor = true;
+            this.buttonFiles.Click += new System.EventHandler(this.buttonFiles_Click);
+            // 
+            // textBoxFiles
+            // 
+            this.textBoxFiles.Location = new System.Drawing.Point(30, 43);
+            this.textBoxFiles.Multiline = true;
+            this.textBoxFiles.Name = "textBoxFiles";
+            this.textBoxFiles.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxFiles.Size = new System.Drawing.Size(371, 190);
+            this.textBoxFiles.TabIndex = 1;
+            this.textBoxFiles.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxFiles.TextChanged += new System.EventHandler(this.textBoxFiles_TextChanged);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRun.Location = new System.Drawing.Point(224, 249);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 2;
+            this.buttonRun.Text = "Convert";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAbout.Location = new System.Drawing.Point(515, 3);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(22, 23);
+            this.buttonAbout.TabIndex = 3;
+            this.buttonAbout.Text = "?";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 307);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.textBoxFiles);
+            this.Controls.Add(this.buttonFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.Text = "Liquid text convertor by EvilCorp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFiles;
+        private System.Windows.Forms.TextBox textBoxFiles;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonAbout;
+    }
+}
+
---
+++ LtxConvertor/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LtxConvertor
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragDrop += new DragEventHandler(MenuDragDrop);
+            this.DragEnter += new DragEventHandler(MenuDragEnter);
+        }
+
+        private string[] files = new string[] { };
+
+        private void MenuDragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void MenuDragEnter(object sender, DragEventArgs e)
+        {
+            try
+            {
+                List<string> newFiles = ((string[])e.Data.GetData(DataFormats.FileDrop)).ToList();
+                newFiles.AddRange(files);
+                files = newFiles.Where(t => t.Substring(t.Length - 3, 3) == "ltx" || t.Substring(t.Length - 3, 3) == "lte").ToArray();
+            }
+            catch
+            {
+                MessageBox.Show("Error adding new files");
+            }
+           
+        }
+
+        private void buttonFiles_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Liquid WOTR|*.ltx|Liquid BattleRealms|*.lte", Multiselect = true, InitialDirectory = Application.StartupPath })
+            {
+                files = ofd.ShowDialog() == DialogResult.OK ? ofd.FileNames : new string[] { };
+                this.textBoxFiles.Lines = files;
+            }
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //about
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxFiles_TextChanged(object sender, EventArgs e)
+        {
+            files = this.textBoxFiles.Lines;
+        }
+    }
+}
